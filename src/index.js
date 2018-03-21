0577dcc08835239e882e53c50fd748b40c861105
Jude <julie_wang1@brown.edu>
2018-03-21 16:07:25 -0400

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './fonts.css';
+import colors from './colors.json';
+
+function header() {
+  return <h3 class="head">timeline · · ·</h3>;
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      score : 0,
+      events : [],
+    };
+  }
+
+  handleClick(app) {
+    this.setState({
+      score: 0,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {header()}
+
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
